bf9dd036bf1faaf396afebf80d135bb7a88ec602
jainikptl <jainikpatel670@gmail.com>
2025-04-12 09:44:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
-const canvas = document.getElementById("bgCanvas");
-const ctx = canvas.getContext("2d");
+  const canvas = document.getElementById("bgCanvas");
+  const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-let particlesArray = [];
+  let particlesArray = [];
 
-class Particle {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 2 + 0.5;
-    this.speedX = Math.random() * 0.6 - 0.3;
-    this.speedY = Math.random() * 0.6 - 0.3;
+  class Particle {
+    constructor() {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+      this.size = Math.random() * 2 + 0.5;
+      this.speedX = Math.random() * 0.6 - 0.3;
+      this.speedY = Math.random() * 0.6 - 0.3;
+    }
+    update() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+      if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+    }
+    draw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fillStyle = "rgba(255, 255, 255, 0.3)";
+      ctx.fill();
+    }
   }
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
 
-    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
-    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+  function initParticles() {
+    particlesArray = [];
+    for (let i = 0; i < 120; i++) {
+      particlesArray.push(new Particle());
+    }
   }
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fillStyle = "rgba(255, 255, 255, 0.3)";
-    ctx.fill();
+
+  function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particlesArray.forEach((p) => {
+      p.update();
+      p.draw();
+    });
+    requestAnimationFrame(animateParticles);
   }
-}
 
-function initParticles() {
-  particlesArray = [];
-  for (let i = 0; i < 120; i++) {
-    particlesArray.push(new Particle());
-  }
-}
+  initParticles();
+  animateParticles();
 
-function animateParticles() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particlesArray.forEach((p) => {
-    p.update();
-    p.draw();
-  });
-  requestAnimationFrame(animateParticles);
-}
-
-initParticles();
-animateParticles();
+  // Remove splash screen after 4 seconds
+  setTimeout(() => {
+    const splash = document.getElementById('splashScreen');
+    if (splash) {
+      splash.style.animation = "fadeOut 1s forwards";
+      setTimeout(() => splash.remove(), 1000); // cleanup from DOM
+    }
+  }, 4000);
